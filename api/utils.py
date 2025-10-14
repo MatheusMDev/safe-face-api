b802e5fd4b378ca6f127f98341624b7649f07303
MatheusMDev <matheu.miranda@hotmail.com>
2025-10-13 23:27:08 -0300

    commit de ajuste final

--- api/utils.py
+++ api/utils.py
@@ -2,12 +2,7 @@
 from PIL import Image
 import numpy as np
 
-<<<<<<< HEAD
 def preprocess_image(image: Image.Image, target_size=(260,260)):
-=======
-#definir tamanho ainda
-def preprocess_image(image: Image.Image, target_size=(160,160)):
->>>>>>> 18c798a874ed7efa0dbb7b69b147b92ad3f3819c
     image = image.resize(target_size)
     image_array = np.array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
